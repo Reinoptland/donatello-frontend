dbe37f7e5df6a91e9cfea79e27161bef88d8138e
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-07-07 11:07:51 +0200

    style wip

---
+++ src/components/BtnCtA.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./BtnCta.css";
+
+const Button = () => {
+  return (
+    <button className="btn btn--cta btn--discover" type="button">
+      Discover Projects
+    </button>
+  );
+};
+
+export default Button;
--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./CallToAction.css";
+import { FaHandHoldingHeart } from "react-icons/fa";
+import Button from "./BtnCtA";
 
 const CallToAction = () => {
-  return <div>Call to Action</div>;
+  const iconStyle = { color: "#730ec3", fontSize: "5em" };
+  return (
+    <div className="main-cta-wrapper">
+      <span classname="icon icon--donate">
+        <FaHandHoldingHeart style={iconStyle} />
+      </span>
+      <section className="text text--big">
+        <h1>Donate to work you support</h1>
+      </section>
+      <section className="text text--small">
+        <h4>Make your checkbook follow your heart</h4>
+      </section>
+      <span className="button">
+        <Button />
+      </span>
+    </div>
+  );
 };
 
 export default CallToAction;
