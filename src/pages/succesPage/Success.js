ccbb69effbc27b094d6733c142c27f885c96e5ba
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-07-09 10:21:57 +0200

    npm install axios and async function

--- src/pages/succesPage/Success.js
+++ src/pages/succesPage/Success.js
@@ -2,6 +2,21 @@ import React from "react";
 import "../succesPage/success.scss";
 import CallToAction from "../../components/CallToAction";
 import { GiShakingHands } from "react-icons/gi";
+import axios from "axios";
+
+const PaymentSucces = () => {
+  async function fetchPayment() {
+    try {
+      const response = await axios.get(
+        `https://donatello-development.herokuapp.com/projects/bf35aad5-1736-4934-b018-5d2bd0c88c63/donations/14212a4f-ccce-45e6-8bbd-6bc220bece69`
+      );
+      console.log("Response", response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  fetchPayment();
+};
 
 const SuccessPage = () => {
   return (
