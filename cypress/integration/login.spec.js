e771df13cc4f41e2dc96c367fd72df7df28b13f2
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-07-14 14:38:58 +0200

    installed eslint plugin and fixed warning

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -18,9 +18,12 @@ describe("Login page functionality", () => {
     cy.get(`[name="email"]`).type("rein@rein.com");
     cy.get(`[name="password"]`).type("abcd1234");
     cy.get(`form`).submit();
+    /* eslint-disable */
     cy.wait("@apiCall").then((interception) => {
       expect(interception.request.body.email).to.include("rein@rein.com");
       expect(interception.request.body.password).to.include("abcd1234");
+      return interception;
     });
+    /* eslint-enable */
   });
 });
