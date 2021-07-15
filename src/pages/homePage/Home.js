f9a299245219b7b220cf8e1bdda41d9fdf39324b
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-07-15 15:22:17 +0200

    add css to homepage

--- src/pages/homePage/Home.js
+++ src/pages/homePage/Home.js
@@ -7,36 +7,44 @@ import ProjectCard from "../../components/ProjectCard";
 
 const Home = () => {
   return (
-    <div className="cta-wrapper">
-      <aside className="cta cta--donate">
-        <CallToAction
-          icon={
-            <FaHandHoldingHeart style={{ color: "#730ec3", fontSize: "5em" }} />
-          }
-          background="white"
-          title="Donate to work you support"
-          description="Make your checkbook follow your heart"
-          button="purple"
-          btnText="Discover Projects"
-        />
-      </aside>
-      <aside className="cta cta--project">
-        <CallToAction
-          background="purple"
-          icon={<GiBiceps style={{ color: "white", fontSize: "5em" }} />}
-          title="Change is in your hands"
-          description="Start a project that changes the world"
-          button="white"
-          btnText="Start a project"
-        />
-      </aside>
-      <div>
-        <ProjectCard
-          titleHeader="Cool animation project"
-          by="by: Arty McArtis"
-        />
-      </div>
-    </div>
+    <>
+      <header className="home__header">
+        <div className="cta-wrapper">
+          <aside className="cta cta--donate">
+            <CallToAction
+              icon={
+                <FaHandHoldingHeart
+                  style={{ color: "#730ec3", fontSize: "5em" }}
+                />
+              }
+              background="white"
+              title="Donate to work you support"
+              description="Make your checkbook follow your heart"
+              button="purple"
+              btnText="Discover Projects"
+            />
+          </aside>
+          <aside className="cta cta--project">
+            <CallToAction
+              background="purple"
+              icon={<GiBiceps style={{ color: "white", fontSize: "5em" }} />}
+              title="Change is in your hands"
+              description="Start a project that changes the world"
+              button="white"
+              btnText="Start a project"
+            />
+          </aside>
+        </div>
+      </header>
+      <main className="home__main">
+        <div>
+          <ProjectCard
+            titleHeader="Cool animation project"
+            by="by: Arty McArtis"
+          />
+        </div>
+      </main>
+    </>
   );
 };
 
